1bec8031f622bf9ea556008ae4448e5dc24f1f94
coutupp <coutup@educ.college-montreal.qc.ca>
2022-09-15 10:14:21 -0400

    TP1

---
+++ main.py
@@ -0,0 +1,12 @@
+#créé par Philippe Coutu
+#15 septembre 2022
+phrase = "mon code est original frong."
+
+def count_words(sentence):
+    nombre = len(sentence.split())
+    return nombre
+print("---------------------------------------------")
+print("| phrase : ", phrase, "       |")
+print("---------------------------------------------")
+print("| nombre de mot dans la phrase : ", count_words(phrase),"|")
+print("---------------------------------------------")
